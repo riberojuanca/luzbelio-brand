74bd1a9ba4cdf6df3d23a3715e1f25672611a3c5
Juanca <riberojuanca@gmail.com>
2024-07-20 18:55:38 -0300

    Added styles in FeaturedProducts

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -1,5 +1,9 @@
 const FeaturedProducts = () => {
-  return <p>Desde Featured Products</p>;
+  return (
+    <div className="max-w-6xl py-4 mx-auto sm:py-16 sm:px-24">
+      <h3 className="px-6 text-3xl sm:pb-8">Productos destacados</h3>
+    </div>
+  );
 };
 
 export default FeaturedProducts;
