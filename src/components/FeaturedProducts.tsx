f16f697f04a3001cfdfa0a534a81b95d0a5b8093
Juanca <riberojuanca@gmail.com>
2024-07-24 10:48:23 -0300

    Editing FeaturedProducts

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -6,15 +6,13 @@ import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 import SkeletonSchema from "./SkeletonSchema";
 import { ProductType } from "../../types/products";
 import { Card, CardContent } from "./ui/card";
-import { Expand, ShoppingCart } from "lucide-react";
+import { DollarSign, DollarSignIcon, Expand, ShoppingCart } from "lucide-react";
 import IconButton from "./IconButton";
 import { useRouter } from "next/navigation";
 
 const FeaturedProducts = () => {
   const { loading, result }: ResponseType = useGetFeaturedProducts();
   const router = useRouter();
-
-  console.log(result);
 
   return (
     <div className="flex flex-col items-center max-w-6xl py-4 mx-auto sm:py-16 sm:px-24">
@@ -31,32 +29,36 @@ const FeaturedProducts = () => {
                   key={id}
                   className=" md:basis-1/2 lg:basis-1/3 group"
                 >
-                  <div className="p-1">
-                    <Card className="py-4 border border-gray-200 shadow-none">
-                      <CardContent className="relative flex items-center justify-center px-6 py-2">
+                  <div className="">
+                    <Card className="border border-gray-200 shadow-none p-2 flex flex-col gap-2">
+                      <CardContent className="relative flex items-center justify-center p-0">
                         <img
+                          className="rounded-lg"
                           src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${images.data[0].attributes.url}`}
                           alt="Image featured"
                         />
-                        <div className="absolute w-full px-6 transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
-                          <div className="flex justify-center gap-x-6">
+                        <div className="absolute w-full transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
+                          <div className="flex justify gap-x-4">
                             <IconButton
                               onClick={() => router.push(`product/${slug}`)}
                               icon={<Expand size={20} />}
                               className="text-gray-600"
                             />
                             <IconButton
-                              onClick={() => console.log("Add item")}
+                              onClick={() => console.log("Click en carrito")}
                               icon={<ShoppingCart size={20} />}
                               className="text-gray-600"
                             />
                           </div>
                         </div>
                       </CardContent>
-                      <div className="flex justify-between gap-4 px-8">
+                      <div className="flex justify-between gap-4">
                         <h3 className="text-base font-semibold">
                           {productName}
                         </h3>
+                        <div className="flex justify-between gap-3">
+                          <p>${price}</p>
+                        </div>
                       </div>
                     </Card>
                   </div>
