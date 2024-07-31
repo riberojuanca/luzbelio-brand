dba7ef7765f391a016639fba868e4892a29447d7
Juanca <riberojuanca@gmail.com>
2024-07-31 18:28:07 -0300

    Corrected FeaturedProducts

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -2,7 +2,13 @@
 "use client";
 import { useGetFeaturedProducts } from "@/api/useGetFeaturedProducts";
 import { ResponseType } from "../../types/response";
-import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
 import SkeletonSchema from "./SkeletonSchema";
 import { ProductType } from "../../types/products";
 import { Card, CardContent } from "./ui/card";
@@ -15,10 +21,10 @@ const FeaturedProducts = () => {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col items-center max-w-6xl py-4 mx-auto sm:py-16 sm:px-24">
-      <h3 className="px-6 text-3xl sm:pb-8">Productos destacados</h3>
-      <Carousel>
-        <CarouselContent className="-ml-2 md:-ml-4">
+    <div className=" flex flex-col items-center max-w-6xl py-4 mx-auto sm:py-16 sm:px-24">
+      <h3 className="px-6 text-lg sm:text-3xl sm:pb-8">Productos destacados</h3>
+      <Carousel className="w-screen sm:w-full">
+        <CarouselContent className=" sm:w-auto -ml-2 md:-ml-4">
           {loading && <SkeletonSchema grid={3} />}
           {result !== null &&
             result.map((product: ProductType) => {
@@ -27,17 +33,17 @@ const FeaturedProducts = () => {
               return (
                 <CarouselItem
                   key={id}
-                  className=" md:basis-1/2 lg:basis-1/3 group"
+                  className="pl-2 md:basis-1/2 lg:basis-1/3 group"
                 >
                   <div className="py-2">
-                    <Card className="shadow-none p-2 flex flex-col gap-2">
+                    <Card className="border-none shadow-none p-2 flex flex-col gap-2">
                       <CardContent className="relative flex items-center justify-center p-0">
                         <img
-                          className="rounded-lg"
+                          className="rounded-lg max object-cover h-40"
                           src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${images.data[0].attributes.url}`}
                           alt="Image featured"
                         />
-                        <div className="absolute w-full transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
+                        <div className="absolute w-full px-6 transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
                           <div className="flex justify-center items-center gap-x-4">
                             <IconButton
                               onClick={() => router.push(`product/${slug}`)}
@@ -66,6 +72,8 @@ const FeaturedProducts = () => {
               );
             })}
         </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext className="hidden sm:flex" />
       </Carousel>
     </div>
   );
